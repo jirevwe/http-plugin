99bd75b36f11e4fb12258ebc80ac8c67e4fe86ef
Raymond Tukpe <rtukpe@gmail.com>
2024-02-13 18:49:04 +0100

    feat: added files

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/jirevwe/http-plugin
+
+go 1.21
+
+require github.com/jirevwe/plug v0.0.0-20240213084333-96df767d63df
+
+require (
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
---
+++ http.go
@@ -0,0 +1,30 @@
+package http_plugin
+
+import (
+	"fmt"
+	"github.com/jirevwe/plug"
+	"time"
+)
+
+func init() {
+	plug.RegisterModule(Http{})
+}
+
+const ID plug.ModuleID = "core.logger"
+
+type Http struct{}
+
+func (h Http) Emit(value any) error {
+	fmt.Printf("[HTTP]: %v", value.(time.Time).Unix())
+	return nil
+}
+
+func (h Http) ModuleInfo() plug.ModuleInfo {
+	return plug.ModuleInfo{
+		ID:  ID,
+		New: func() plug.Module { return new(Http) },
+	}
+}
+
+// var _ plug.Validator = (Http)(nil)
+var _ plug.Emitter = (*Http)(nil)
